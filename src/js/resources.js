046218d0cd7e13a9c322064300e3dd4250c1e29b
Jadybaby <1103637@hr.nl>
2026-06-16 14:01:15 +0200

    images toegevoegd.

--- src/js/resources.js
+++ src/js/resources.js
@@ -3,17 +3,27 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 // voeg hier jouw eigen resources toe
 const Resources = {
     Player: new ImageSource('images/karakterCLE1.png'),
+    Ghost: new ImageSource('images/ghost-loneliness.png'),
+    Shadow: new ImageSource('images/shadowForward.png'),
+    ShadowLeft: new ImageSource('images/shadowLeft.png'),
+    ShadowRight: new ImageSource('images/shadowRight.png'),
+
     Map1: new ImageSource('images/map1.png'),
     Map2: new ImageSource('images/map2.png'),
     Map3: new ImageSource('images/map3.png'),
     Map4: new ImageSource('images/map4.png'),
 
+    FullHeart: new ImageSource('images/Full_heart.png'),
+    EmptyHeart: new ImageSource('images/Empty_heart.png'),
+
     Fireball: new ImageSource('images/fireballPowerup.gif'),
     Light: new ImageSource('images/lightPower.gif'),
 
     Eenzaamheid: new ImageSource('images/Puzzlestuk_Eenzaamheid.png'),
-    Stress: new ImageSource('images/Puzzlestuk_Stress.png'),
-    Puzzelstuk3: new ImageSource('images/Puzzelstuk3.png'),
+    Stress: new ImageSource('images/Puzzlestuk_stress.png'),
+    Angst: new ImageSource('images/Puzzelstuk_angst.png'),
+    Zelftwijfel: new ImageSource('images/Puzzelstuk_zelftwijfel.png'),
+    Schaamte: new ImageSource('images/Puzzelstuk_schaamte.png'),
 
     StartScreen: new ImageSource('images/startScreen.png'),
 }
