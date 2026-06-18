6b4169ce5ce2f15fed8b81ebc0a6669e7266bdd0
KashmiraW <Kashmira.walidin@gmail.com>
2026-06-18 11:25:55 +0200

    ADDED_key

--- src/js/level4.js
+++ src/js/level4.js
@@ -2,7 +2,7 @@ import { Scene, Actor, Vector, Camera } from "excalibur";
 import { Resources } from "./resources.js";
 import { Player } from "./Player.js";   
 import { Chest } from "./chest.js";
-
+import { Key }  from "./key.js";
 
 export class LevelFour extends Scene {
 
@@ -23,15 +23,22 @@ export class LevelFour extends Scene {
         this.add(this.chest);
 
         this.chest.pos = new Vector(640, -325);
-        this.chest.scale = new Vector(0.4, 0.4);
         this.chest.z = 1;
 
         this.chest = new Chest();
         this.add(this.chest);
 
-        this.chest.pos = new Vector(950, 1210);
-        this.chest.scale = new Vector(0.4, 0.4);
+        this.chest.pos = new Vector(750, 1170);
         this.chest.z = 1;
+
+        this.Key = new Key();
+        this.add(this.Key);
+        this.Key.pos = new Vector(950, 200);
+
+
+        this.Key = new Key();
+        this.add(this.Key);
+        this.Key.pos = new Vector(250, 800);
 
         this.camera.strategy.lockToActor(this.player);
     }
