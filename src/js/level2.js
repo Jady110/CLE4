4ff1477b3d010461191900e8e49616b4ff60ccfd
Jadybaby <1103637@hr.nl>
2026-06-25 13:54:44 +0200

    ja idk wat ik dacht maar ik heb letterlijk niks gedaan..

--- src/js/level2.js
+++ src/js/level2.js
@@ -66,9 +66,17 @@ export class LevelTwo extends Scene {
         this.chest.pos = new Vector(1325, 650);
         this.add(this.chest);
 
-        this.powerup = new PowerLaughter();
-        this.powerup.pos = new Vector(650, 750);
-        this.add(this.powerup);
+        this.powerup1 = new PowerLaughter();
+        this.powerup1.pos = new Vector(650, 750);
+        this.add(this.powerup1);
+
+        this.powerup2 = new PowerLaughter();
+        this.powerup2.pos = new Vector(1050, 30);
+        this.add(this.powerup2);
+
+        this.powerup3 = new PowerLaughter();
+        this.powerup3.pos = new Vector(-230, 800);
+        this.add(this.powerup3);
 
         this.enemy1 = new ShadowEnemy();
         this.enemy1.pos = new Vector(250, -50);
