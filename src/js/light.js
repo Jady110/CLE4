21e1d566b43ce7c0b6542bf44354191e0de7033a
kellseyvdongen <kellseyvandongen2007@gmail.com>
2026-06-29 14:29:43 +0200

    height en width van powers verwijderd

--- src/js/light.js
+++ src/js/light.js
@@ -4,8 +4,6 @@ import { Resources } from "./Resources.js";
 export class Light extends Actor {
     constructor() {
         super({
-            width: 200,
-            height: 1000
         });
         this.scale = new Vector(0.3, 0.3);
         this.body.collisionType = CollisionType.Passive;
